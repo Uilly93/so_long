387faec019f6812403b224dde7ddc4e376e6c4e5
wnocchi <wnocchi@student.42.fr>
2024-02-06 17:14:11 +0100

    realloc added, needed for parsing

--- libft/ft_realloc.c
+++ libft/ft_realloc.c
@@ -6,20 +6,23 @@
 /*   By: wnocchi <wnocchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 17:00:08 by wnocchi           #+#    #+#             */
-/*   Updated: 2024/02/06 17:08:56 by wnocchi          ###   ########.fr       */
+/*   Updated: 2024/02/06 17:14:06 by wnocchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*realloc(void *ptr, size_t size)
+void	*ft_realloc(void *ptr, size_t size)
 {
 	void *reallocated;
 
-	reallocated = ft_calloc(1, sizeof(void *));
-	if(!reallocated)
+	reallocated = ft_calloc(1, size);
+	if (!reallocated)
 		return (NULL);
-	ft_memcpy(reallocated, (void *)ptr, size);
-	free(ptr);
+	if (ptr)
+	{
+		ft_memcpy(reallocated, (void *)ptr, size);
+		free(ptr);
+	}
 	return (reallocated);
 }
